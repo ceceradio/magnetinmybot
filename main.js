d2acd2b0f99f2c8a7bbf297712cf948c842e191f
= <charles.wagner@devfluid.com>
2015-02-19 00:07:05 +0000

    trying to find nounsgit add .

--- main.js
+++ main.js
@@ -1,1 +1,19 @@
+var config = require('./config.js');
+var twitter = require('twitter');
+var fs = require('fs');
+var twit = new twitter(config.twitter);
+
 // nothing yet
+var data = {q:"in my"}
+twit.get('search/tweets', data, function(error, tweets, response){
+    if(error) { console.log("Error:"); console.log(error); return; }
+    if (tweets.statuses.length > 0) {
+        for (var tweetIndex in tweets.statuses) {
+            var tweet = tweets.statuses[tweetIndex];
+            console.log(tweet.text.match(/in my (\w)+/g));
+            
+        }
+    }
+    else
+        console.log("No tweets found.");
+});
