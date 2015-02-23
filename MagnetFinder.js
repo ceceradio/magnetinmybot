771756cd1704bcf64146a588c086118168fcf8fb
= <charles.wagner@devfluid.com>
2015-02-23 23:41:42 +0000

    seperated string functions out to a testable module.

---
+++ MagnetFinder.js
@@ -0,0 +1,89 @@
+var fs = require('fs');
+var MagnetFinder = function () {
+    this.nouns = {};
+    this.adjectives = {};
+    this.usedLocations = {};
+}
+    // dictionary provided by http://icon.shef.ac.uk/Moby/mpos.html
+    MagnetFinder.prototype.loadDictionary = function(callback) {
+        var self = this;
+        fs.readFile("mobyposi.i", {encoding:"utf8"}, function(err, data) {
+            if (err) {
+                console.log(err); callback(err); return;
+            }
+            var lines = data.split("\r");
+            for (var i in lines) {
+                var parts = lines[i].split(/[\uFFFC-\uFFFD]/);
+                if (parts.length < 2) {
+                    continue;
+                }
+                if (parts[1].indexOf("A") > -1)
+                    self.adjectives[parts[0]] = true;
+                if (parts[1].indexOf("N") > -1)
+                    self.nouns[parts[0]] = true;
+            }
+            callback(false);
+        });
+    }
+    MagnetFinder.prototype.initializeDictionaries = function(callback) {
+        var self = this;
+        this.loadUsedLocations(function(err) {
+            self.loadDictionary(function(err) {
+                if (err) { callback(err); return; }
+                callback(false);
+            });
+        });
+    }
+    
+    MagnetFinder.prototype.loadUsedLocations = function (callback) {
+        var self = this;
+        fs.readFile("saveData.json", 'utf-8', function(err,data) {
+            if (err) {
+                callback(err);
+                return;
+            }
+            self.usedLocations = JSON.parse(data);
+            callback(false);
+        });
+    }
+    MagnetFinder.prototype.addUsedLocation = function(location) {
+        this.usedLocations[location] = true;
+        this.saveUsedLocations(function(err){
+            if (err) {console.log(err);return;}
+            console.log("Saved");
+        });
+    }
+    MagnetFinder.prototype.saveUsedLocations = function (callback) {
+        fs.writeFile('saveData.json', JSON.stringify(this.usedLocations), {}, function(err) {
+            if (err) { console.log(err); callback(err); return; }
+            callback(false);
+        });
+    }
+    MagnetFinder.prototype.findWhereMyMagnetIs = function (text) {
+        // i can make this recursive but i don't want to think about it right now
+        var potentialNouns = text.match(/in my (\w+)/);
+        if (potentialNouns !== null) {
+            if (potentialNouns[1] in this.adjectives) {
+                var potentialNouns2 = text.match(/in my (\w+) (\w+)/);
+                if (potentialNouns2 !== null) {
+                    if (potentialNouns2[2] in this.adjectives) {
+                        var potentialNouns3 = text.match(/in my (\w+) (\w+)/);
+                        if (potentialNouns3 !== null) {
+                            if (potentialNouns3[3] in this.nouns && !((potentialNouns3[1]+" "+potentialNouns3[2]+" "+potentialNouns3[3]).toLowerCase() in this.usedLocations))
+                                return potentialNouns3[1]+" "+potentialNouns3[2]+" "+potentialNouns3[3];
+                        }
+                    }
+                    if (potentialNouns2[2] in this.nouns && !((potentialNouns2[1]+" "+potentialNouns2[2]).toLowerCase() in this.usedLocations))
+                        return potentialNouns2[1]+" "+potentialNouns2[2];
+                }
+            }
+            if (potentialNouns[1] in this.nouns && !(potentialNouns[1].toLowerCase() in this.usedLocations)) {
+                return potentialNouns[1];
+            }
+        }
+        return false;
+    }
+
+if (typeof module !== "undefined") {
+    module.exports = MagnetFinder;
+}
--- main.js
+++ main.js
@@ -1,88 +1,38 @@
 var config = require('./config.js');
+var MagnetFinder = require('./MagnetFinder.js');
+var magnetFinder = new MagnetFinder();
 var twitter = require('twitter');
-var prompt = require('prompt');
 var fs = require('fs');
 var twit = new twitter(config.twitter);
 
-var nouns = {};
-var adjectives = {};
+
 var badWords = {};
-var usedLocations = {};
 var tweetInterval = 2 * 60 * 1000;
 
-loadUsedLocations(function(err) {
-    loadDictionary(function(err) {
+loadBadwordDictionary(function(err) {
+    magnetFinder.initializeDictionaries(function(err) {
         if (err) return;
-        loadBadwordDictionary(function(err) {
-            if (err) return;
-            setInterval(function() {doTweet();}, tweetInterval);
-            doTweet();
-        });
+        setInterval(function() {doTweet();}, tweetInterval);
+        doTweet();
     });
 });
 
-
 function doTweet() {
     pickAMagnetLocation(function(err, magnetLocation) {
+        if (err) {console.log("doTweet Error: "+err); return;}
+        if (typeof magnetLocation === "undefined" || magnetLocation === false || magnetLocation === "") {
+            console.log("No locations found"); return;
+        }
         var tweetData = {status: "I put a magnet in my "+magnetLocation};
         twit.post('statuses/update', tweetData, function(error, body, response) {
             if(error) {console.log("doTweet Error:"); console.log(error); return;}
-            usedLocations[magnetLocation.toLowerCase()] = true;
+            magnetFinder.addUsedLocation(magnetLocation.toLowerCase());
             console.log("I put a magnet in my "+magnetLocation);
-            saveUsedLocations(function() {console.log("Saved");});
         });
     });
 }
 
-// dictionary provided by http://icon.shef.ac.uk/Moby/mpos.html
-function loadDictionary(callback) {
-    fs.readFile("mobyposi.i", {encoding:"utf8"}, function(err, data) {
-        if (err) {
-            console.log(err); callback(err); return;
-        }
-        var lines = data.split("\r");
-        for (var i in lines) {
-            var parts = lines[i].split('�');
-            if (parts.length < 2) {
-                continue;
-            }
-            if (parts[1].indexOf("N") > -1)
-                nouns[parts[0]] = true;
-            else if (parts[1].indexOf("A") > -1)
-                adjectives[parts[0]] = true;
-        }
-        callback(false);
-    });
-}
-function loadBadwordDictionary(callback) {
-	fs.readFile("stopwords.txt", 'utf-8', function(err,data) {
-		if (err) {
-			callback(err);
-			return;
-		}
-		var stopwordsArray = data.split(/\s+/);
-		for (var i=0;i<stopwordsArray.length;i++) {
-			badWords[stopwordsArray[i]] = true;
-		}
-        callback(false);
-	});
-}
-function loadUsedLocations(callback) {
-	fs.readFile("saveData.json", 'utf-8', function(err,data) {
-		if (err) {
-			callback(err);
-			return;
-		}
-		usedLocations = JSON.parse(data);
-        callback(false);
-	});
-}
-function saveUsedLocations(callback) {
-    fs.writeFile('saveData.json', JSON.stringify(usedLocations), {}, function(err) {
-        if (err) { console.log(err); callback(err); return; }
-        callback(false);
-    });
-}
+
 function findPotentialMagnetLocations(callback) {
     var data = {q:"in my", count: 100}
     fullTexts = [];
@@ -102,7 +52,7 @@ function pickAMagnetLocation(callback) {
         if (texts.length == 0) { console.log("No locations"); callback(true); return; }
         var magnetLocations = [];
         for (var i in texts) {
-            var magnetLocation = findWhereMyMagnetIs(texts[i]);
+            var magnetLocation = magnetFinder.findWhereMyMagnetIs(texts[i]);
             if (magnetLocation !== false && !hasBadWords(magnetLocation)) {
                 magnetLocations.push(magnetLocation);
             }
@@ -110,21 +60,18 @@ function pickAMagnetLocation(callback) {
         callback(false, magnetLocations[Math.floor(magnetLocations.length * Math.random())]);
     });
 }
-function findWhereMyMagnetIs(text) {
-    var potentialNouns = text.match(/in my (\w+)/);
-    if (potentialNouns !== null) {
-        if (potentialNouns[1] in adjectives) {
-            potentialNouns2 = text.match(/in my (\w+) (\w+)/);
-            if (potentialNouns2 !== null) {
-                if (potentialNouns2[2] in nouns && !(potentialNouns2[1]+" "+potentialNouns2[2]).toLowerCase() in usedLocations)
-                    return potentialNouns2[1]+" "+potentialNouns2[2];
-            }
+function loadBadwordDictionary(callback) {
+    fs.readFile("stopwords.txt", 'utf-8', function(err,data) {
+        if (err) {
+            callback(err);
+            return;
         }
-        if (potentialNouns[1] in nouns && !(potentialNouns[1].toLowerCase() in usedLocations)) {
-            return potentialNouns[1];
+        var stopwordsArray = data.split(/\s+/);
+        for (var i=0;i<stopwordsArray.length;i++) {
+            badWords[stopwordsArray[i]] = true;
         }
-    }
-    return false;
+        callback(false);
+    });
 }
 function hasBadWords(text) {
     var tokens = text.split(/\s+/);
