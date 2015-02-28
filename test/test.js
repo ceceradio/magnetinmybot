b7fe7ca2c50f5ef76c95c71b1be8c4ec61bd8277
= <charles.wagner@devfluid.com>
2015-02-28 23:11:53 +0000

    added a test that is failing right now but should not

--- test/test.js
+++ test/test.js
@@ -27,12 +27,14 @@ describe('MagnetFinder', function() {
             magnetFinder.initializeDictionaries(function() {
                 assert.notDeepEqual(magnetFinder.nouns, {});
                 assert.notDeepEqual(magnetFinder.adjectives, {});
+                magnetFinder.usedLocations = {};
                 done();
             });
         });
         it('Should return longer sentences before shorter ones', function() {
             assert.equal(magnetFinder.findWhereMyMagnetIs("in my cool smelly car"),"cool smelly car");
-           
+            // this is failing right now, but shouldn't.
+            assert.equal(magnetFinder.findWhereMyMagnetIs("yeah in my car in my cool smelly car"),"cool smelly car");
         });
     });
 });
